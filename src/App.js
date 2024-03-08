0da119ca48622495ad76b586f2b78a2f1908cba6
TSchmitt <schm4798@kettering.edu>
2024-03-08 11:44:37 -0500

    added Footer. Cleaned up console errors

--- src/App.js
+++ src/App.js
@@ -5,23 +5,23 @@ import Home from './home-component/Home.js'
 function App() {
   return (
     <div className="App">
-      <head>
+      {/* <head>
 
         <meta charset="utf-8"></meta>
         <title>Vinyl Destination</title>
         <base href="/"></base>
         <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
         <link rel="icon" type="image/x-icon" href="favicon.ico"></link>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-      </head>
-      <body style={{backgroundColor: "bisque"}}>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossOrigin="anonymous"></link>
+      </head> */}
+      <div style={{backgroundColor: "bisque"}}>
         <Home></Home>
-      </body>
+      </div>
     
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
        <h1>My app is working</h1>
-      </header>
+      </header> */}
     </div>
   );
 }
--- src/home-component/Home.js
+++ src/home-component/Home.js
@@ -1,20 +1,39 @@
 export default function Home() {
     return (
-    <header class="head" style={{backgroundColor: "bisque"}}>
-        <div class="row">
-            <div class="large-6" style={{textAlign: "center"}}>
-            <h1 class="font-test">Vinyl Destination</h1>
+        <div>
+    <header className="head" style={{backgroundColor: "bisque"}}>
+        <div className="row">
+            <div className="large-6" style={{textAlign: "center"}}>
+            <h1 className="font-test">Vinyl Destination</h1>
             </div>
         </div>
-        <div class="row page-header"> 
-            <div class="col-2 nav-button"><p></p></div>
-            <div class="col-2 nav-button"><a href="./"> <h2>All Vinyls</h2></a></div>
-            <div class="col-2 nav-button"><a class="board" routerLink="/next" routerLinkActive="active" ariaCurrentWhenActive="page"><h2>60s</h2></a></div>
-            <div class="col-2 nav-button"><a class="board" routerLink="/next" routerLinkActive="active" ariaCurrentWhenActive="page"><h2>70s</h2></a></div>
-            <div class="col-2 nav-button"><a class="board" routerLink="/next" routerLinkActive="active" ariaCurrentWhenActive="page"><h2>80s</h2></a></div> 
-            <div class="col-2 nav-button" style={{paddingTop: "0.5rem"}}><a href="./login"><h5>Log in/Sign up</h5></a></div> 
+        <div className="row page-header"> 
+            <div className="col-2 nav-button"><p></p></div>
+            <div className="col-2 nav-button"><a href="./"> <h2>All Vinyls</h2></a></div>
+            <div className="col-2 nav-button"><a className="board" ><h2>60s</h2></a></div>
+            <div className="col-2 nav-button"><a className="board" ><h2>70s</h2></a></div>
+            <div className="col-2 nav-button"><a className="board" ><h2>80s</h2></a></div> 
+            <div className="col-2 nav-button" style={{paddingTop: "0.5rem"}}><a href="./login"><h5>Log in/Sign up</h5></a></div> 
         </div> 
 
     </header> 
+
+    <footer className="footer-outline">
+        <div className="row">
+            <div className="col-2 footer-col">
+                <div className="row"><h5>Visit Us Here</h5></div>
+                <div className="row"><a>Donate Here</a></div>
+                <div className="row"><a>Social Media Links</a></div>
+                <div className="row"><a>Other</a></div>
+            </div>
+            <div className="col-2 footer-col">
+                <div className="row"><h5>Account Links</h5></div>
+                <div className="row"><a href="./login" >Log in</a></div>
+                <div className="row"><a>Account Settings</a></div>
+                <div className="row"><a>Sign Out</a></div>
+            </div>
+        </div>
+    </footer>
+    </div>
     );
 }
