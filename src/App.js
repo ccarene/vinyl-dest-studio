74748896958d16aa614bc29a1f81230d8f316e5e
ccarene <care2472@kettering.edu>
2024-03-08 11:45:24 -0500

    revert App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
-import Home from './home-component/Home.js'
+import {
+  ActionCard 
+ } from './ui-components';
 
 function App() {
   return (
     <div className="App">
-      <head>
-
-        <meta charset="utf-8"></meta>
-        <title>Vinyl Destination</title>
-        <base href="/"></base>
-        <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
-        <link rel="icon" type="image/x-icon" href="favicon.ico"></link>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
-      </head>
-      <body style={{backgroundColor: "bisque"}}>
-        <Home></Home>
-      </body>
-    
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-       <h1>My app is working</h1>
+        <p>
+          <ActionCard />
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
